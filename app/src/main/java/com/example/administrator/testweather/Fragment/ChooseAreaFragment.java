128d9454e9d65e0f8a2c81449f311833e19f86f3
Lhaiyu <1255759571@qq.com>
2018-05-18 10:08:42 +0800

    second commit

---
+++ app/src/main/java/com/example/administrator/testweather/Fragment/ChooseAreaFragment.java
@@ -0,0 +1,11 @@
+package com.example.administrator.testweather.Fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Administrator on 2018/5/18.
+ */
+
+public class ChooseAreaFragment extends Fragment {
+
+}
